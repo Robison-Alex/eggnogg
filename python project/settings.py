27d65fd8f9dd09ac7760eec3c1d0393c8dfa4ef7
Robison-Alex <113559768+Robison-Alex@users.noreply.github.com>
2023-03-08 15:12:13 -0800

    Add files via upload

---
+++ python project/settings.py
@@ -0,0 +1,31 @@
+LEVEL_MAP = [
+'XX                          ',
+'XXXX                        ',
+'XX                     Q     ',
+'       XXXX           XX    ',
+'                            ',
+'XXXXX         XX         XX ',
+' XXXX   P   XX              ',
+' XX    XXXXXXX  XXXXXXXXXX  ',
+'       XXXXXXX    XXXXXXXX  ',
+'    XXXXXXXXXXXX  XXXXXXXX  ',
+'XXXXXXXXXXXXXXXX  XXXXXXXX  ']
+# '0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20' *64 to get it the levels x and y pos
+TILE_SIZE = 64
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+# Colors
+BG_COLOR = '#060C17'
+PLAYER_COLOR = '#C4F7FF'
+PLAYER2_COLOR = 'white'
+SWORD_COLOR = 'red'
+TILE_COLOR = '#94D7F2'
+
+# Camera
+CAMERA_BORDERS = {
+    'left': 100,
+    'right': 200,
+    'top': 100,
+    'bottom': 150
+}
